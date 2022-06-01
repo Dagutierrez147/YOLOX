2d8fd9d07e68def8a54f81023268250b3053ec98
Diego Gutiérrez <74571253+Dagutierrez147@users.noreply.github.com>
2022-05-31 20:44:02 -0400

    Update yolox_s.py
    Change default path

--- exps/example/custom/yolox_s.py
+++ exps/example/custom/yolox_s.py
@@ -14,7 +14,7 @@ class Exp(MyExp):
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
         # Define yourself dataset path
-        self.data_dir = "datasets/coco128"
+        self.data_dir = "/opt/YOLOX/datasets/coco128"
         self.train_ann = "instances_train2017.json"
         self.val_ann = "instances_val2017.json"
 
